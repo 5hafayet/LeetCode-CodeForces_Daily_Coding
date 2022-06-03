371e4926280172cc39d5119885c83979d69a5fc0
5hafayet <shafayetahmad1@gmail.com>
2022-06-03 23:24:45 +0600

    new solutions added

---
+++ CodeForces/1678A_Tokitsukaze_and_all_zero_sequence.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void changeNumber(vector<int> arr_)
+{
+    for (int i = 0; i < arr_.size(); i++)
+    {
+        /* code */
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t)
+    {
+        int n;
+        vector<int> arr;
+        cin >> t;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        t--;
+    }
+}
---
+++ CodeForces/1691C_Sum_Of_SubStrings.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sumOfStr(char arr[])
+{
+    int sum = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        /* code */
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t)
+    {
+        int n, k;
+        string s;
+        vector<char> arr;
+        cin >> n >> k;
+        cin >> s;
+        strcpy(arr, s.c_str());
+
+        t--;
+    }
+}
