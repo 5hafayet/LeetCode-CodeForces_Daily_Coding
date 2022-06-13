170ce14e3535f9f9f7e858bf77ded184e6452375
5hafayet <shafayetahmad1@gmail.com>
2022-06-13 23:44:39 +0600

    new leetcode codeforces added

---
+++ CodeForces/112A_petya_and_string.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(string str1, string str2)
+{
+    vector<char> str3;
+    vector<char> str4;
+    int x = 0;
+    int y = 0;
+    for (int i = 0; i < str1.size(); i++)
+    {
+        str3.push_back(toupper(str1[i]));
+        str4.push_back(toupper(str2[i]));
+    }
+
+    for (int i = 0; i < str3.size(); i++)
+    {
+        x += str3[i];
+        y += str4[i];
+    }
+
+    if (x > y)
+        return 1;
+    else if (x == y)
+        return 0;
+    else
+        return -1;
+}
+
+int main()
+{
+    string str1, str2;
+    cin >> str1;
+    cin >> str2;
+    cout << solution(str1, str2) << endl;
+}
